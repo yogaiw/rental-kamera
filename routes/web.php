2fb703e30038cc238665025c87b239e5d3c0b22c
yogaiw <agoy.indra@gmail.com>
2022-02-13 22:17:35 +0700

    homecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\KameraController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[AuthController::class, 'index'])->name('login');
+Route::get('/',[HomeController::class, 'index'])->name('home');
 Route::post('/login',[AuthController::class, 'authenticate']);
 
 Route::middleware(['auth','admin'])->group(function () {
