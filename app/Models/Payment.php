a9f60e655b035a4cdc9513513aaea9f73cd1a003
yogaiw <agoy.indra@gmail.com>
2022-04-14 00:09:03 +0700

    minor relationship

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    public function order() {
+        return $this->hasMany(Order::class, 'payment_id', 'id');
+    }
 }
