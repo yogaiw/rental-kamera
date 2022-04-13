a07c1174adc9b2977269e49b2c0eb7a572a24a8f
yogaiw <agoy.indra@gmail.com>
2022-04-13 16:16:27 +0700

    fix issue in previous commit

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,20 +11,21 @@ class OrderController extends Controller
 {
     public function create(Request $request) {
         $cart = Carts::where('user_id', Auth::id())->get();
-        $orders = new Order();
 
         foreach($cart as $c) {
-            $orders->no_invoice = "test aja";
-            $orders->alat_id = $c->alat_id;
-            $orders->user_id = $c->user_id;
-            $orders->start_date = $request['start_date'];
-            $orders->start_time = $request['start_time'];
-            $orders->end_date = date('Y-m-d', strtotime($request['start_date'] . "+".$c->durasi." hours"));
-            $orders->end_time = date('H:i', strtotime($request['start_date'] . "+".$c->durasi." hours"));
-            $orders->harga = $c->harga;
-
-            $orders->save();
+            Order::create([
+                'no_invoice' => "test aja",
+                'alat_id' => $c->alat_id,
+                'user_id' => $c->user_id,
+                'start_date' => $request['start_date'],
+                'start_time' => $request['start_time'],
+                'end_date' => date('Y-m-d', strtotime($request['start_date'] . "+".$c->durasi." hours")),
+                'end_time' => date('H:i', strtotime($request['start_date'] . "+".$c->durasi." hours")),
+                'harga' => $c->harga,
+            ]);
+            $c->delete();
         }
+
         return redirect(route('member.index'));
     }
 }
