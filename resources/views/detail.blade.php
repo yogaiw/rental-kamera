1f1075a9f401da24f86f428f4b78bfa29bb32b31
yogaiw <agoy.indra@gmail.com>
2022-06-03 15:07:00 +0700

    minor

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -11,7 +11,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     </head>
     <body>
-        <div class="container-fluid">
+        <div class="container">
             <div class="row justify-content-center mt-4">
                 <div class="col-md-12 col-lg-4">
                     <div class="card mb-4">
