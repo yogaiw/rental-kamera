357eda1df6c0bfb4977d29630e27fca2b4692556
yogaiw <agoy.indra@gmail.com>
2022-06-22 19:59:26 +0700

    data type changes on role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('telepon')->nullable();
-            $table->boolean('role')->default(0);
+            $table->tinyInteger('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
